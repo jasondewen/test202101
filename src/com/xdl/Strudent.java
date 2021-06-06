52e050d2c2b4cb180cbf421a9db4dcf955e68c3b
sdw <jasondewen@163.com>
2021-06-06 14:08:56 +0800

    123

---
+++ src/com/xdl/Strudent.java
@@ -0,0 +1,27 @@
+package com.xdl;
+
+/**
+ * @author sdw
+ * @date 2021/6/6  -- 13:52
+ */
+public class Strudent {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
